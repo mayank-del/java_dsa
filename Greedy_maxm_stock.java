6445e063d6ab6e04ffe25744b70454bc54c9882e
mayank-del <82238106+mayank-del@users.noreply.github.com>
2023-12-24 17:34:33 +0530

    potd 24/12/23

---
+++ Greedy_maxm_stock.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            String str=br.readLine();
+    		String[] starr=str.split(" ");
+    		
+    		//input n and d
+    	    int n=Integer.parseInt(starr[0]);
+    		int k= Integer.parseInt(starr[1]);
+            
+            starr = br.readLine().trim().split(" ");
+            int[] price = new int[n];
+            for(int i = 0; i < n; i++)
+                price[i] = Integer.parseInt(starr[i]);
+            
+            Solution obj = new Solution();
+            int res = obj.buyMaximumProducts(n, k, price);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Pair{
+    int data;
+    int maxm_limit;
+    
+    Pair(int data,int maxm_limit){
+        this.data=data;
+        this.maxm_limit=maxm_limit;
+    }
+}
+class Solution {
+    public static int buyMaximumProducts(int n, int k, int[] price) {
+        // code here
+        Pair[] p=new Pair[n];
+        for(int i=0;i<price.length;i++){
+            p[i]=new Pair(price[i],i+1);
+        }
+        
+        Arrays.sort(p,(a,b)->a.data-b.data);
+        int count=0;
+        
+        for(int i=0;i<p.length;i++){
+            for(int j=0;j<p[i].maxm_limit;j++){
+                if(p[i].data<=k){
+                    k-=p[i].data;
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
+        
