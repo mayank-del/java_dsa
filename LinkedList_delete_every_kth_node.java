733cfeaab26baa783a49816d81f5268ae1804031
mayank-del <82238106+mayank-del@users.noreply.github.com>
2024-04-29 18:24:12 +0530

    potd 29/4/24

---
+++ LinkedList_delete_every_kth_node.java
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node
+{
+	Node next;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+class Delete_Kth_Node
+{
+	Node head;
+	Node tail;
+	
+	void addToTheLast(Node node){
+		if(head == null){
+		head = node;
+		tail = node;
+		}
+		else
+		{
+		    tail.next = node;
+		    tail = tail.next ;
+		}
+	}	
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();		 
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			Delete_Kth_Node list = new Delete_Kth_Node();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            list.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				list.addToTheLast(new Node(a));
+			}
+			int k = sc.nextInt();					  
+			//System.out.println(list.head.data+" "+ k);
+			list.head = new Solution().delete(list.head,k); 
+			Node temp = list.head;
+			while(temp!=null)
+			{
+				System.out.print(temp.data+ " ");
+			    temp = temp.next;
+			}
+			System.out.println();
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/* Link list Node
+class Node
+{
+	Node next;
+	int data;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+class Solution
+{
+    /*You are required to complete this method*/
+    Node delete(Node head, int k)
+    {
+	// Your code here
+	if(k==1) return null;
+	
+	int cnt=0;
+	if(k==2)
+	    cnt=0;
+	 else cnt=2;
+	    Node temp=head;
+	    while( temp!=null){
+	        if(cnt==0 && k==2){
+	            temp.next=temp.next.next;
+	            cnt+=2;
+	        }
+	       else if(cnt!=0 && cnt%k==0 && temp.next!=null ){
+	           temp.next=temp.next.next;
+	           cnt+=2;
+	       }
+	       else{
+	           
+	            cnt++;
+	       }
+	       temp=temp.next;
+	    }
+	    
+	        
+	        
+	    return head;
+    }
+}
