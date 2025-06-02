f4ab056740a880452fecc8f2e86ecd5a3551439d
mayank-del <82238106+mayank-del@users.noreply.github.com>
2025-06-02 23:37:27 +0530

    amazon q1

--- Recursion_Amazon__maxm_median_of_subseq_of_size_k.java
+++ Recursion_Amazon__maxm_median_of_subseq_of_size_k.java
@@ -1,49 +1,9 @@
-import java.util.*;
-/* //method overloading practice
-
-class Play{
-    
-    public static String meth1(int num){
-        String res="Returned integer is:"+num;
-        return res;
-    }
-    public static String meth1(int num,boolean b){
-        String res="Returned integer is:"+num+","+"Status is:"+b;
-        return res;
-    }
-}
-class HelloWorld {
-    public static void main(String[] args) {
-        Play p=new Play();
-        String stored=p.meth1(6,false);
-        System.out.print(stored);
-    }
-}
-
-
-//Arraylist 2D update method 
-
-
-
 import java.util.ArrayList;
-import java.util.Scanner;
-class HelloWorld {
-    public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
-        arr.add(new ArrayList<>());
-        Scanner sc=new Scanner(System.in);
-        for(int i=0;i<5;i++){
-            System.out.print("Enter your number:");
-            int data=sc.nextInt();
-            arr.get(0).add(data);
-        }
-        arr.get(0).set(2,785878);
-        for(Integer it:arr.get(0)){
-            System.out.print(it+",");
-        }
-    }
-} */
-class Practice_part{
+import java.util.Collections;
+import java.util.List;
+
+public class Recursion_Amazon__maxm_median_of_subseq_of_size_k {
+    
     static int maxMedian = Integer.MIN_VALUE;
 
     public static void main(String[] args) {
@@ -80,3 +40,5 @@ class Practice_part{
         f(arr, index + 1, current, k);
     }
 }
+
+
